f3f26e9da8770367c682d9f177c4e9fd94a97ca0
Harshvardhan Aditya <harshva27@gmail.com>
2022-11-25 09:19:11 +0530

    New Commit

--- s1-hessian-matrix/src/App.js
+++ s1-hessian-matrix/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState ,useEffect} from "react";
+import React from "react"; 
+import Axios from 'axios';
+import $ from 'jquery';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   // set 
+//   const [blogs, setBlogs] = useState(null);
+//   const YOUR_API_KEY='CVAeJ2DxqjKRLzjWj65o6fuY-aMLEkG5Q4MeLXZBr58';
+//   // const smd='random_joke';
+//   const getJoke=()=>{
+//     Axios.get('https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey='+YOUR_API_KEY+'&c=52.431,13.3845&z=16').then(
+//       (response)=>{
+//         console.log(response);
+//       }
+//     );
+//   }
+//   return (
+//     <div>
+//       <button onClick={getJoke}>Hello world</button>
+//       <img src="" alt="" />
+//     </div>
+//   );
+// }
 
+// export default App;
+
+//App.js
+// import React, { useState } from 'react';
+import {DisplayMapClass} from './DisplayMapClass';
+import data from './API.json';
+
+function App() {
+  let textInput = React.createRef();
+  const [loc,setLoc]=useState('Pune');
+  function handleClick() {
+  console.log(textInput.current.value);
+  setLoc(textInput.current.value);
+
+}
+  
+  useEffect(()=>{
+    let f=loc;
+    
+    // for
+    Axios.get('https://geocode.search.hereapi.com/v1/geocode?q=240+Washington+St.%2C+Boston&apiKey='+data.YOUR_API_KEY).then((response)=>{
+      // setLoc(response); 
+      console.log(response);
+    })},[loc]); 
+return (
+  <div>
+      <input ref={textInput} placeholder="Type a message..." />
+      <button onClick={handleClick}>Search Location</button>
+  <DisplayMapClass />
+  </div>
+  
+  
+);
+}
 export default App;
